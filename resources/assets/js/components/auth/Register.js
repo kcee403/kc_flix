080d3cb30ee8991afb780c50fbc4e1553134079e
kcee403 <kcee403@gmail.com>
2018-05-24 23:18:51 -0400

    SITE API CALL

--- resources/assets/js/components/auth/Register.js
+++ resources/assets/js/components/auth/Register.js
@@ -65,7 +65,7 @@ class Register extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    const INMOTION = 'https://kcflix-api.herokuapp.com/api/users/register';
+    const INMOTION = 'https://www.kcflix.site/api/users/register';
 
     const user = {
       name: this.state.name,
@@ -79,7 +79,7 @@ class Register extends Component {
           console.log(user.name);
           console.log(INMOTION);
 
-    axios.post('https://kcflix-api.herokuapp.com/api/users/register', user)
+    axios.post('https://www.kcflix.site/api/users/register', user)
       .then(res => {
         console.log(res);
         console.log(res.data);
