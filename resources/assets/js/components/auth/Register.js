3fb4b179e8535a0d3c6146ec10e5a3f37105d25f
kcee403 <kcee403@gmail.com>
2018-05-24 22:51:59 -0400

    node modules, git ignore

--- resources/assets/js/components/auth/Register.js
+++ resources/assets/js/components/auth/Register.js
@@ -79,7 +79,7 @@ class Register extends Component {
           console.log(user.name);
           console.log(INMOTION);
 
-    axios.post('https://www.kcflix.site/api/users/register', user)
+    axios.post('www.kcflix.site/api/users/register/', user)
       .then(res => {
         console.log(res);
         console.log(res.data);
